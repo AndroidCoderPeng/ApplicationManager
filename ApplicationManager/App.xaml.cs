e9f7c19f9de2782e993af96daadf09570d681a4e
pengxianhong <290677893@qq.com>
2024-04-30 14:25:57 +0800

    First Commit

---
+++ ApplicationManager/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using Prism.DryIoc;
+using Prism.Ioc;
+
+namespace ApplicationManager
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void InitializeShell(Window shell)
+        {
+            base.InitializeShell(shell);
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+    }
+}
---
+++ ApplicationManager/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.Windows.Interop;
+using System.Windows;
+using System;
+using System.IO.Ports;
+
+namespace ApplicationManager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            Hide();
+            e.Cancel = true;
+        }
+    }
+}
